0173a8b6a269be7de2b1e816e55629d001145951
Tinotenda Joel Muchenje <tinotendam@ajs.co.za>
2020-07-30 22:42:25 +0200

    Added calculations

--- Fitness-SPA/src/app/survey/survey.component.ts
+++ Fitness-SPA/src/app/survey/survey.component.ts
@@ -10,15 +10,55 @@ export class SurveyComponent implements OnInit {
 
   constructor() { }
 
+  // tslint:disable-next-line: typedef
   ngOnInit() {
   }
 
+  // tslint:disable-next-line: typedef
   calculate(){
+    this.model.goal = true;
+    console.log(this.model);
+    const gender = this.model.gender;
+    let female;
+    let mantainanceDailyCalories;
+    let activeDailyCalories;
+    let protein;
+    let carbohydrates;
+    let fat;
 
+    if (gender === 'female'){
+       female = (this.model.weight * 10) + (this.model.height * 100 * 6.5) - (this.model.age * 5) - 161;
+       mantainanceDailyCalories = female * this.model.activityLevel;
+       activeDailyCalories = mantainanceDailyCalories * 0.75;
+      }
+
+    let male;
+    if (gender === 'male'){
+      male = (this.model.weight * 10) + (this.model.height * 100 * 6.5) - (this.model.age * 5) + 5;
+      mantainanceDailyCalories = male * this.model.activityLevel;
+      activeDailyCalories = mantainanceDailyCalories * 1.25;
+    }
+
+    protein = (activeDailyCalories * 20 / 100) / 4;
+    protein = (protein / activeDailyCalories) * 100;
+
+    carbohydrates = (activeDailyCalories * 50 / 100) / 4;
+    carbohydrates = (carbohydrates / activeDailyCalories) * 100;
+
+    fat = (activeDailyCalories * 30 / 100) / 9;
+    fat = (fat / activeDailyCalories) * 100;
+
+    this.model.protein = Math.round(protein);
+    this.model.carbohydrates = Math.round(carbohydrates);
+    this.model.fat = Math.round(fat);
     console.log(this.model);
   }
 
-  hasLanded(){
+  // tslint:disable-next-line: typedef
+  hasLanded(gender){
+    console.log('has landed clicked');
     this.model.change = true;
+    this.model.goal = false;
+    this.model.gender = gender;
   }
 }
