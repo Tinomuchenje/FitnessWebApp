14b01ae3860ac9f72c0c08a8bda069b84afc18a4
Tinotenda Joel Muchenje <tinotendam@ajs.co.za>
2020-08-09 14:53:58 +0200

    Added prgress on navigation
    - Removed unecessary code.
    - Added pregress bar which changes depending on where the child router is on.

--- Fitness-SPA/src/app/form-bar/form-bar.component.ts
+++ Fitness-SPA/src/app/form-bar/form-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-bar',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-bar.component.css']
 })
 export class FormBarComponent implements OnInit {
+  route: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(location: Location, router: Router) {
+    router.events.subscribe(val => {
+      if (location.path() !== '') {
+        this.route = location.path();
+        console.log(this.route);
+      }
+    });
+   }
+  ngOnInit(): void {
   }
-
 }
