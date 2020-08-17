673bc87481abdad630033be84b06098bcf20ace7
Tinotenda Joel Muchenje <tinotendam@ajs.co.za>
2020-08-17 23:27:06 +0200

    Passed selected plan title
    - removed demo button

--- Fitness-SPA/src/app/payment/payment.component.ts
+++ Fitness-SPA/src/app/payment/payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-payment',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentComponent implements OnInit {
 
+  @Input() selectedCourseTitle = '';
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit(): void {
 
+  }
 }
