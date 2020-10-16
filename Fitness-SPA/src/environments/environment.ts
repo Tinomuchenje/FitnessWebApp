efc2bffa5909e7eb081b31b6b9094fde87723565
Tinotenda Joel Muchenje <tinotendam@ajs.co.za>
2020-10-16 07:39:10 +0200

    Changed environment for production test

--- Fitness-SPA/src/environments/environment.ts
+++ Fitness-SPA/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyCXz0aUd6VqtvkKMFsbDx0MbtXpGyO8APg',
     authDomain: 'fitness-775ef.firebaseapp.com',
